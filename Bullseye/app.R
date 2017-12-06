51b9a6ee70bfc9d30d6d601ccbdc7d2948ec5a82
jh-206 <jhirschi@GreenfieldAdvisors.com>
2017-12-06 11:52:48 -0800

    Test: app input

--- Bullseye/app.R
+++ Bullseye/app.R
@@ -3,37 +3,70 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+  
+  DT::dataTableOutput("lags", width = 300), tags$hr(),
    
+  selectInput(inputId = "lag_input", label = "Lag", choices = 1:100),
+  
+  actionButton("record", "Record")
+  
    # Application title
-   titlePanel("Bullseye Project"),
+   # titlePanel("Bullseye Project"),
    
    # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-     sidebarPanel(
-       selectInput(inputId = "lag_input", label = "Lag", choices = 1:100)
-     ),
+   # sidebarLayout(
+   #   sidebarPanel(
+   #     selectInput(inputId = "lag_input", label = "Lag", choices = 1:100)
+   #   ),
       
-      # Show a plot of the generated distribution
-      mainPanel(
-        tabsetPanel(
-          type = "tab",
-          tabPanel("Data Collection",
-                   textOutput("tracker")
-          ),
-          tabPanel("Analysis",
-                   tableOutput("bullseye_table")
-          )
-        )
-      )
-   )
+      # mainPanel(
+      #   tabsetPanel(
+      #     type = "tab",
+      #     tabPanel("Data Collection",
+      #              textOutput("tracker")
+      #     ),
+      #     tabPanel("Analysis",
+      #              tableOutput("bullseye_table")
+      #     )
+      #   )
+      # )
+   # )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  saveData <- function(data) {
+    data <- as.data.frame(t(data))
+    if (exists("lags")) {
+      lags <<- rbind(lags, data)
+    } else {
+      lags <<- data
+    }
+  }
+  
+  loadData <- function() {
+    if (exists("lags")) {
+      lags
+    }
+  }
+  
+  lagData <- reactive({
+    input$lag_input
+  })
+  
+  observeEvent(input$submit, {
+    saveData(lagData())
+  })
+  
+  output$lags <- DT::renderDataTable({
+    input$submit
+    loadData()
+  })     
    
-   output$tracker <- renderText({
-     print(input$lag_input)
-   })
+   # output$tracker <- renderText({
+   #   print(input$lag_input)
+   # })
    
 }
 
